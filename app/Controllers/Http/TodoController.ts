8db566b850e2bfb22d88aea703d413f188395cac
darrius <mohamedtine17@gmail.com>
2023-12-14 11:33:01 +0000

    fixed bug in todo's controller

--- app/Controllers/Http/TodoController.ts
+++ app/Controllers/Http/TodoController.ts
@@ -1,8 +1,10 @@
 import type { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import Todo from "App/Models/Todo";
+import Project from "App/Models/Project";
 
 export default class TodoController {
   public async index(ctx: HttpContextContract) {
+    //console.log(Project.$getRelation("todo").relatedModel());
     try {
       let todo = await Todo.all();
       return { todo };
@@ -13,19 +15,20 @@ export default class TodoController {
 
   public async create({ request }: HttpContextContract) {
     let data = request.body();
-    try {
-      await Todo.create({
-        content: data.content,
-        stat: false,
-      });
+    let project_id = request.param("id");
+    let project = await Project.findOrFail(project_id);
+    let todo = await project.related("todo").create({
+      content: data.content,
+    });
+    if (todo.project_id == project.id) {
       return { message: "Success" };
-    } catch {
+    } else {
       return { message: "Failed" };
     }
   }
 
   public async update({ request }: HttpContextContract) {
-    let todo = request.param("id");
+    let todo = request.param("id_todo");
     let body = request.body();
     try {
       const data = await Todo.findOrFail(todo);
@@ -38,9 +41,9 @@ export default class TodoController {
   }
 
   public async delete({ request }: HttpContextContract) {
-    let todo = request.param("id");
+    let todo_id = request.param("id_todo");
     try {
-      const data = await Todo.findOrFail(todo);
+      const data = await Todo.findOrFail(todo_id);
       await data.delete();
       return { message: "Success" };
     } catch {
--- app/Models/Todo.ts
+++ app/Models/Todo.ts
@@ -1,9 +1,17 @@
-import { BaseModel, column } from "@ioc:Adonis/Lucid/Orm";
+import { BaseModel, column, BelongsTo, belongsTo } from "@ioc:Adonis/Lucid/Orm";
+import Project from "./Project";
 
 export default class Todo extends BaseModel {
   public static table = "todo";
+
   @column({ isPrimary: true })
   public id: number;
+
+  @column()
+  public project_id: number;
+
+  @belongsTo(() => Project)
+  public project: BelongsTo<typeof Project>;
 
   @column()
   public content: string;
